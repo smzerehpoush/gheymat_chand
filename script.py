24aeff9ff7c7fa8fbf268e30e2ee022f530c50c8
Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2024-08-01 00:18:32 +0330

    add tala.ir

--- script.py
+++ script.py
@@ -2,8 +2,6 @@ import gc
 import os
 import time
 import traceback
-
-import redis
 import requests
 from khayyam import JalaliDatetime
 
@@ -11,13 +9,12 @@ profile = os.environ.get('GHEYMAT_CHAND_PROFILE')
 bot_token = os.environ.get('BOT_TOKEN')
 chat_id = os.environ.get('CHAT_ID')
 private_chat_id = os.environ.get('PRIVATE_CHAT_ID')
-redis_connection = redis.Redis(host='redis', port=6379, db=0)
 
 lastPrice = 0
 url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
 
 
-def get_milli_prices():
+def get_milli_price():
     try:
         milli_response = requests.get("https://milli.gold/api/v1/public/milli-price/detail", timeout=1)
         if (milli_response.status_code != 200):
@@ -25,27 +22,41 @@ def get_milli_prices():
             requests.post(url, data={'chat_id': private_chat_id,
                                      'text': f'Milli Gold error:\n {milli_response.status_code} \n {milli_response.content}'},
                           timeout=1)
-            return None, None
+            return None
         milli_price = int(milli_response.json()['price18'] * 100)
-        return milli_price, milli_price
+        return milli_price
     except  Exception:
         traceback.print_exc()
-        return None, None
+        return None
+    
+def get_tala_dot_ir_price():
+    try:
+        tala_dot_ir_response = requests.get("https://www.tala.ir/ajax/price/talair", timeout=1)
+        if (tala_dot_ir_response.status_code != 200):
+            print(f'milli error:\n{tala_dot_ir_response.content}')
+            requests.post(url, data={'chat_id': private_chat_id,
+                                     'text': f'Milli Gold error:\n {tala_dot_ir_response.status_code} \n {tala_dot_ir_response.content}'},
+                          timeout=1)
+            return None
+        tala_dot_ir_price = int(tala_dot_ir_response.json()['gold']['gold_18k']['v'])
+        return tala_dot_ir_price
+    except  Exception:
+        traceback.print_exc()
+        return None
 
 
 while True:
     try:
         now = JalaliDatetime.now()
         timestamp = int(time.time())
-        print(f'start at {timestamp}')
+        print(f'start at {now}')
         prices = []
         text = ''
         prices.append(('قیمت طلا 🟡', ''))
-        milli_buy_price, milli_sell_price = get_milli_prices()
-        if milli_buy_price and milli_sell_price:
-            prices.append(('میلی', f'{milli_buy_price:,} - {milli_sell_price:,}'))
-            redis_connection.set('milli_price', str(milli_buy_price))
-
+        milli_price = get_milli_price()
+        prices.append(('میلی', f'{milli_price:,}'))
+        tala_dot_ir_price = get_tala_dot_ir_price()
+        prices.append(('طلا دات آی آر', f'{tala_dot_ir_price:,}'))
         for name, price in prices:
             text += f"<code>{name}{' ' * (10 - len(name))}{price}</code>\n"
 
@@ -66,7 +77,7 @@ while True:
             else:
                 print(text)
         lastPrice = price
-        time.sleep(30)
+        time.sleep(20)
         gc.collect()
     except Exception as e:
         traceback.print_exc()
