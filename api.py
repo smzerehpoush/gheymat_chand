6f2352dd047e161bd084fcfd3812aadb538a98f0
Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2024-10-24 23:11:41 +0330

    get last n items

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request  # Import request to handle query parameters
 import redis
 import json  # Import the json module to handle JSON strings
 
@@ -12,19 +12,23 @@ redis_connection = redis.Redis(host='redis', port=6379, db=1)
 REDIS_KEY = 'prices_history'
 
 @app.route('/gold/api/prices', methods=['GET'])
-def get_last_30_prices():
+def get_last_n_prices():
     try:
-        # Fetch the last 30 items from the Redis list
-        last_30_items = redis_connection.lrange(REDIS_KEY, -60, -1)
+        # Get the 'n' query parameter from the request, default to 30 if not provided
+        n = request.args.get('n', default=30, type=int)
+        
+        # Fetch the last 'n' items from the Redis list
+        # Using -n for the start index to get the last n prices
+        last_n_items = redis_connection.lrange(REDIS_KEY, -n, -1)
         
         # Decode the bytes data to strings (assuming the data is stored as JSON strings)
-        last_30_items = [item.decode('utf-8') for item in last_30_items]
+        last_n_items = [item.decode('utf-8') for item in last_n_items]
         
         # Convert the JSON strings to Python objects
-        last_30_items = [json.loads(item) for item in last_30_items]
+        last_n_items = [json.loads(item) for item in last_n_items]
 
         # Return the data as a JSON response
-        return jsonify({'dataset': last_30_items}), 200
+        return jsonify({'dataset': last_n_items}), 200
 
     except Exception as e:
         # Handle exceptions and return an error message
